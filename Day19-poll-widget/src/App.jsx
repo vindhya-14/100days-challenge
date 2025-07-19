f7d2e04d15d7909ba0fca39a6020dda426026177
Vindhya <vindhya3321@gmail.com>
2025-07-19 22:19:50 +0530

    Day-19-poll-widget

---
+++ Day19-poll-widget/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Poll from './components/Poll';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Poll />
+    </div>
+  );
+}
+
+export default App;
