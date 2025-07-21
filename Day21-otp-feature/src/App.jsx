a77b6324e4e0657c11db83c3e5a45deb593dc1fb
Vindhya <vindhya3321@gmail.com>
2025-07-21 23:30:06 +0530

    Day-21

---
+++ Day21-otp-feature/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import OtpInput from "./component/OtpInput";
+
+
+function App() {
+  const handleOtpSubmit = (otp) => {
+    alert("OTP entered: " + otp);
+  };
+
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h2>Enter OTP</h2>
+      <OtpInput length={6} onOtpSubmit={handleOtpSubmit} />
+    </div>
+  );
+}
+
+export default App;
