5ca6c2674f93b807ec5e806439f7f23e979619c2
Vindhya <vindhya3321@gmail.com>
2025-07-31 20:50:17 +0530

    Day 31-drag -drop

---
+++ Day31-Drag-Drop-Feature/src/components/Task.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Draggable } from "react-beautiful-dnd";
+
+function Task({ task, index }) {
+  return (
+    <Draggable draggableId={task.id} index={index}>
+      {(provided) => (
+        <div
+          className="task"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          {task.content}
+        </div>
+      )}
+    </Draggable>
+  );
+}
+
+export default Task;
